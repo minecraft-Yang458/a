4ab98a2286185a1e0c459da1b23763dddb4d522e
minecraft-Yang458 <18457376055@163.com>
2025-08-23 15:50:53 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,6 @@
-<!-- /config.js -->
-<script>
-// 在全站任何页面中作为只读配置使用
-window.SITE_CONFIG = Object.freeze({
-  TEAM_NAME: "七三大全管理团队",
-  VERSION: "Beta 1.2.0",
-  COPYRIGHT_SINCE: 2023, // 版权起始年，用于显示“2023–当前年”
-
-  CONTACT: {
-    email: "18457376055@163.com",
-    qqGroup: "1048229673"
-  },
-
-  LICENSE: {
-    name: "CC BY 4.0",
-    url: "https://creativecommons.org/licenses/by/4.0/deed.zh"
-  }
-});
-</script>
+window.SITE_CONFIG = {
+    TEAM_NAME: "七三大全管理团队",
+    VERSION: "Beta 1.2.0",
+    EMAIL: "18457376055@163.com",
+    QQ_GROUP: "1048229673"
+};
