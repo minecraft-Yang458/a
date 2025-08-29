17544969155e0188bf1204a951541a513953707a
minecraft-Yang458 <18457376055@163.com>
2025-08-29 14:02:15 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.SITE_CONFIG = {
     TEAM_NAME: "七三大全管理团队",
     VERSION: "Beta 1.3.0",
-    EMAIL: "18457376055@163.com",
+    EMAIL: "admin@ft123.dpdns.org",
     QQ_GROUP: "1048229673"
 };
